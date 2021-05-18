c35ceb482cefa088bdd26a0286f8009ceded6d21
MD <mattiadamio@gmail.com>
2021-05-18 12:48:18 +0200

    creazione del menu nel main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,6 +15,18 @@ public class Main
 {
     public static void main(String[] args) 
     {
+        String[] vociMenu=new String[7];
+        int sceltaUtente=-1;
+        Scanner tastiera=new Scanner(System.in);
         
+        
+        vociMenu[0]="Esci";
+        vociMenu[1]="Aggiungi vendita";
+        vociMenu[2]="Rimuovi vendita ";
+        vociMenu[3]="Paga vendita non ancora pagata";
+        vociMenu[4]="Visualizza tutte le vendite ";
+        vociMenu[5]="Visualizza i dati delle vendite di un cliente";
+        vociMenu[6]="Esporta in CSV le vendite";
+        vociMenu[7]="Salva su file binario";
     }
 }
